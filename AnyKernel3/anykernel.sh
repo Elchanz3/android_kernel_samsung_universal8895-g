84fb32410ba300a773c3d06b699b5f501e901c69
snapdragon teaks <inutilidades639@gmail.com>
2025-11-04 02:43:23 -0300

    add AnyKernel3

---
+++ AnyKernel3/anykernel.sh
@@ -0,0 +1,48 @@
+### AnyKernel3 Ramdisk Mod Script
+## osm0sis @ xda-developers
+
+### AnyKernel setup
+# global properties
+properties() { '
+kernel.string=kernel by Chanz22 @ xda-developers
+do.devicecheck=1
+do.modules=0
+do.systemless=1
+do.cleanup=1
+do.cleanuponabort=0
+device.name1=greatlte
+supported.versions=
+supported.patchlevels=
+supported.vendorpatchlevels=
+'; } # end properties
+
+
+### AnyKernel install
+## boot files attributes
+boot_attributes() {
+set_perm_recursive 0 0 755 644 $RAMDISK/*;
+set_perm_recursive 0 0 750 750 $RAMDISK/init* $RAMDISK/sbin;
+} # end attributes
+
+# boot shell variables
+BLOCK=/dev/block/platform/11120000.ufs/by-name/BOOT;
+IS_SLOT_DEVICE=0;
+RAMDISK_COMPRESSION=auto;
+PATCH_VBMETA_FLAG=auto;
+
+# import functions/variables and setup patching - see for reference (DO NOT REMOVE)
+. tools/ak3-core.sh;
+
+# boot install
+split_boot; # use split_boot to skip ramdisk unpack, e.g. for devices with init_boot ramdisk
+
+flash_boot; # use flash_boot to skip ramdisk repack, e.g. for devices with init_boot ramdisk
+## end boot install
+
+# reset for init_boot patching
+#reset_ak;
+
+# init_boot install
+#split_boot; # unpack ramdisk since it is the new first stage init ramdisk where overlay.d must go
+
+
